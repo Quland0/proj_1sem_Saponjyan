9f0f2e4d48b31aafc743532e7ad23b87ff7b74b7
Quland0 <pizzabeach4@gmail.com>
2022-11-19 15:25:07 +0300

    pz_6

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,14 +1,18 @@
 #Дан список размера N и целые числа K и L (1 < K < L < N). Найти сумму элементов
-#списка с номерами от K до L включительно.
+#списка с номерами от K до L включительно
+import random
 
 
-N = int(input('введите размер списка: '))
-K = int(input('введите K: '))
-L = int(input('введите L: '))
-list1 = []
-w = 0
-for i in range(N):
-    list1.append(int(input()))
+N = int(input("Введите кол-во элементов массива: "))
+lst = [random.randint(1, 100) for _ in range(N)]
+
+print(f"Список {lst}")
+
+K = int(input("Введите K: "))
+L = int(input("Введите L: "))
+
+sum_ = 0
 for i in range(K, L+1):
-    w += list1[i]
-print(w)
+    sum_ += lst[i]
+
+print(f"Сумма элементов с {K} по {L} позиции = {sum_}")
--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,2 +1,15 @@
 #Дан целочисленный список размера N. Найти количество различных элементов в
-#данном списке.
+#данном списке
+
+import random
+from collections import Counter
+
+
+N = int(input("Введите кол-во элементов массива: "))
+lst = [random.randint(1, 100) for _ in range(N)]
+print(lst)
+
+cnt = Counter(lst)
+
+for key, value in cnt.items():
+    print(f"Число {key} кол-во вхождений в список {value}")
